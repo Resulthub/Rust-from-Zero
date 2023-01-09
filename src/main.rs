76f7df6bcbb106b9e4c82b5d158182aca64782fc
Sadiq <sadik.ahmad@itcglobal.ng>
2023-01-09 12:43:10 +0100

    variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 19;
+    let msg = "Hello, world!";
+    
+
+    println!("x = {}", x);
+    println!("{}", msg);
+
+    // x = x - 3;
+
+    // println!("x = {}", x);
 }
