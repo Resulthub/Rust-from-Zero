872a32e4ab02e7edafc89d37bcc38c327bc4a88a
Sadiq <sadik.ahmad@itcglobal.ng>
2023-01-10 09:24:37 +0100

    loops

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 fn main() {
-    let x = 10;
-
-    if x == 9
-    {
-        println!("If condition is working ")
-    }
-    else if x ==  10
-    {
-        println!("Else if condition is working ")
-    }
-    else
-    {
-        println!("Else condition is working ")
+    //For Loop
+    for x in 0..10 {
+        println!("x = {}", x);
     }
 
+    //while Loop
+    let mut y = 0;
+    while y < 10 {
+        println!("y = {}", y);
+        y += 1;
+    }
+
+    //Do while Loop
+    let mut z = 0;
+    loop {
+        println!("z = {}", z);
+        z += 1;
+        if z == 10 {
+            break;
+        }
+    }
+
+    //For each Loop
+    let a = [10, 20, 30, 40, 50];
+    for i in a.iter() {
+        println!("i = {}", i);
+    }
+
+    
 }
