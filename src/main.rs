1acc66b4ed67133519f2f850e07fcb42d4be675e
Sadiq <sadik.ahmad@itcglobal.ng>
2023-01-10 09:39:10 +0100

    Enum Types

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,19 @@
-fn main() {
-    //For Loop
-    for x in 0..10 {
-        println!("x = {}", x);
-    }
-
-    //while Loop
-    let mut y = 0;
-    while y < 10 {
-        println!("y = {}", y);
-        y += 1;
-    }
-
-    //Do while Loop
-    let mut z = 0;
-    loop {
-        println!("z = {}", z);
-        z += 1;
-        if z == 10 {
-            break;
-        }
-    }
-
-    //For each Loop
-    let a = [10, 20, 30, 40, 50];
-    for i in a.iter() {
-        println!("i = {}", i);
-    }
-
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
     
+fn main() {
+    
+    let player_direction: Direction = Direction::Up;
+
+    match player_direction {
+        Direction::Up => println!("Player is moving up"),
+        Direction::Down => println!("Player is moving down"),
+        Direction::Left => println!("Player is moving left"),
+        Direction::Right => println!("Player is moving right"),
+    }
+
 }
