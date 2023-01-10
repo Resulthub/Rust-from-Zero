5977eebda1e60ba5fb33fefe7a30257bec563f29
Sadiq <sadik.ahmad@itcglobal.ng>
2023-01-10 11:36:34 +0100

    code blocks

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,10 @@
 fn main() {
-    print_numbers_to(20)
+    let x = 19;
 
+    {
+        let y = 16;
 
-}
-
-fn print_numbers_to(num: i32) {
-    for i in 1..num {
-        if is_even(i) {
-            println!("{} is even", i)
-        } else {
-            println!("{} is odd", i)
-        }
+        println!("x = {}, y = {}", x, y);
     }
-}
-
-fn is_even(num: i32) -> bool {
-    return num % 2 == 0
+    
 }
