a5e783d38c792e40d433e495b807d856abb62756
Sadiq <sadik.ahmad@itcglobal.ng>
2023-01-10 09:59:37 +0100

    Tuples

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-const MAXIMUM_NUMBER: u32 = 10;    
 fn main() {
+    let tup1 = (500, 6.4, 1);
+    let tup2 = (19, 09, 96, ("sept", "fri"));
+    let (x, y, z) = tup1;
+
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    println!("The value of z is: {}", z);
+
+    println!("The value of tup1 is: {}", tup1.2);
+    println!("The value of tup2 is: {}", (tup2.3).0);
+
+
+
     
-   for n in 0..MAXIMUM_NUMBER {
-       println!("n is {}", n);
-   }
 }
