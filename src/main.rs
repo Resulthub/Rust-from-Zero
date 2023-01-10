7b9ce6f75615c0aae8e1b3b0555afbbc7cc0998b
Sadiq <sadik.ahmad@itcglobal.ng>
2023-01-10 10:28:03 +0100

    function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 fn main() {
-    let tup1 = (500, 6.4, 1);
-    let tup2 = (19, 09, 96, ("sept", "fri"));
-    let (x, y, z) = tup1;
-
-    println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
-    println!("The value of z is: {}", z);
-
-    println!("The value of tup1 is: {}", tup1.2);
-    println!("The value of tup2 is: {}", (tup2.3).0);
+    print_numbers_to(20)
 
 
+}
 
-    
+fn print_numbers_to(num: i32) {
+    for i in 1..num {
+        if is_even(i) {
+            println!("{} is even", i)
+        } else {
+            println!("{} is odd", i)
+        }
+    }
+}
+
+fn is_even(num: i32) -> bool {
+    return num % 2 == 0
 }
