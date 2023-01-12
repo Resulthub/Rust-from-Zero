b59b9406172f2646592297dbf1c1222117db6bb7
Sadiq <sadik.ahmad@itcglobal.ng>
2023-01-12 10:44:44 +0100

    random

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,7 @@
-use std::collections::HashMap;
-fn main() {
-    let mut marks = HashMap::new();
+extern crate rand;
+use rand::Rng;
 
-    // Inserting values in HashMap
-    marks.insert("Rust", 95);
-    marks.insert("Python", 90);
-    marks.insert("Java", 80);
-    marks.insert("C", 85);
-
-    // Find length of HashMap
-    println!("How many Lanaguages have you studied? {}", marks.len()); 
-
-    // Get a single value
-    println!("What is your marks in Rust? {}", marks.get("Rust").unwrap());
-
-    // Get match
-    match marks.get("Python") {
-        Some(mark) => println!("Your marks in Python is {}", mark),
-        None => println!("You have not studied Python"),
-    }
-
-    // Check if a key exists
-    if marks.contains_key("Java") {
-        println!("You have studied Java");
-    }
-
-    // Check for value
-    println!("Did you study C++ {}", marks.contains_key("C++"));
-
-    // Loop through HashMap
-    for (key, value) in &marks {
-        println!("For {} you got {}%!", key, value);
-    }
-
-    // Remove a value
-    marks.remove("C");
-    println!("{:?}", marks);
-    
-
+fn main(){
+    let random_number = rand::thread_rng().gen_range(1, 11);
+    println!("Random number: {}", random_number);
 }
