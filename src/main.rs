e2f71db44df4e63cb6547d6e81f2e8dd426ad4d6
Sadiq <sadik.ahmad@itcglobal.ng>
2023-01-13 14:37:22 +0100

    regex

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-mod dfile;
+extern crate regex;
+use regex::Regex;
+
 
 fn main(){
-    dfile::print_message();
+    let re = Regex::new(r"(\w{5})").unwrap();
+
+    let text = "hello";
+
+    // println!("Found match? {}", re.is_match(text));
+    match re.captures(text) {
+        // Some(caps) => println!("Found match: {}", caps.get(0).unwrap().as_str()),
+        Some(caps) => println!("Found match: {}", &caps[0]),
+        None => println!("No match found.....")
+    }
 }
