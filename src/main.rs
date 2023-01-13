ebfc23115df1e0ae5894a854507b3d07c8ba731a
Sadiq <sadik.ahmad@itcglobal.ng>
2023-01-13 13:53:56 +0100

    string methods

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,58 @@
-extern crate rand;
-use rand::Rng;
-
 fn main(){
-    let random_number = rand::thread_rng().gen_range(1..11);
-    println!("Random number: {}", random_number);
+    // Replace
+    {
+        let my_string = String::from("Rust is awesome!");
+        println!("After replace: {}", my_string.replace("awesome", "great"));
+    }
+
+    // Lines
+
+    {
+        let my_string = String::from("The quick brown fox \njumps over \nthe lazy dog.");
+        for line in my_string.lines() {
+            println!("[ {} ]", line);
+        } 
+    }
+
+    // Split
+
+    {
+        let my_string = String::from("Leave+a+like+if+you+enjoyed+the+video!");
+        let tokens: Vec<&str> = my_string.split("+").collect();
+
+        println!("{}", my_string);
+        println!("At index 2: {}", tokens[0]);    
+    }
+
+    // Trim
+
+    {
+        let my_string = String::from("  My name is Sadik kabir Ahmad  \n\r");
+        println!("Before trim: {}", my_string);
+        println!("After trim: {}", my_string.trim());
+    }
+
+    // Trim matches
+
+    {
+        let my_string = String::from("Rust is awesome!");
+        println!("Before trim matches: {}", my_string);
+        println!("After trim matches: {}", my_string.trim_matches('!'));
+    }
+
+
+    // Chars
+    {
+        let my_string = String::from("Rust is awesome!");
+
+        println!("{}", my_string);
+
+        // Get characters at index
+
+        match my_string.chars().nth(3) {
+            Some(c) => println!("Character at index 4: {}", c),
+            None => println!("No character at index 4")
+        }
+    }
 
 }
