ab0b325f244a185298a2ced5c3cbddebf1161005
Sadiq <sadik.ahmad@itcglobal.ng>
2023-01-16 09:57:54 +0100

    reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,7 @@
+extern crate reqwest;
+
 fn main() {
-    // let name = String::from("Sadik kabir Ahmad");
 
-    // println!("Character at index 6: {}", match name.chars().nth(6){
-    //     Some(c) => c.to_string(),
-    //     None => "Not found".to_string(),
-    // })
+    match reqest::get("http")
 
-    println!("Occupation is {}", match get_occupation("Ahmad"){
-        Some(r) => r,
-        None => "Not occupation found",
-    })
-
-}
-
-fn get_occupation(name: &str) -> Option<&str> {
-    match name {
-        "Sadik" => Some("Programmer"),
-        "Kabir" => Some("Programmer"),
-        _ => None,
-    }
 }
